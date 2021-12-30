8d647b08f5f21494b36b14e9c2ffe81ce464e239
fahad <iammfa08@gmail.com>
2021-12-30 20:58:23 +0600

    h

--- test.js
+++ test.js
@@ -33,13 +33,13 @@ const pretty = require("pretty");
   $ = cheerio.load(neurl.data);
   const data = $('script').html()
   var text = data.toString()
-  const take = esprima.parseScript(text)
+  //const take = esprima.parseScript(text)
   //console.log(text,JSON.stringify(take))
   var rePattern = new RegExp(/(https:\/\/rr1---sn-[^\s]+[A-D])/gm)
   const reg = text.match(rePattern)
   console.log(reg)
   const regular = /(https:\/\/rr1---sn-[^\s]+[A-D])/gm
-  const read = text.match(regular).toString()
+  const read = text.match(regular)
   //console.log(read)
   console.log('published')
   }

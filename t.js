dd72908b7a6af8bf0666f34f7f37dcabf7d207a7
fahad <iammfa08@gmail.com>
2021-12-30 19:26:37 +0600

    hi

---
+++ t.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs")
+const pretty = require("pretty");
+
+app.get('/',(req,res)=>{
+  res.end(`
+  <h1>Hello World!</h1>
+  `)
+})
+
+app.get('/tv.m3u',async function(req,res) {
+  
+  const url = "https://www.bdstory.xyz/search/label/khorkuto?m=1"
+  const resou = await axios.get(url)
+  var $ = cheerio.load(resou.data);
+  const link = $("#Blog1 > div.blog-posts.hfeed.container.post-filter-wrap > div > article > div > div.FreeGalxzw-box > h2 > a").attr('href')
+  console.log(`todays link ${link}`)
+  
+  const date = new Date().getDate()
+  
+  const ress = await axios.get(link)
+  $ = cheerio.load(ress.data);
+  const post = $("#Blog1 > div > article > div.post-inner-area > div.post-snip > span.post-date.published").text()
+  const pb_pst = post.split(" ")[1].split(',')[0]
+  console.log(pb_pst,date)
+  if(pb_pst != date){
+    console.log('not published')
+    const regt = 'https://github.com/sploit30/emni/blob/main/Untitled%2011%201280x720%201.42Mbps%202021-12-30%2017-16-47.mp4?raw=true'
+    res.writeHead(200,{
+     'Content-Type':'application/x-mpegURL'
+    })
+    res.end(`#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/en/4/42/Khorkuto.jpg" group-title="Bangladesh",Khorkuto
+    ${regt}
+    `)
+    
+  }else{
+  const next = $("body").find("#BLOGGER-video-fbd948bdff01633a-15883").attr("src")
+  console.log(next)
+  const re = await axios.get(next)
+  $ = cheerio.load(re.data);
+  const data = $('script').html()
+  var text = data.toString()
+  const reg = /(https:\/\/rr1---sn-[^\s]+[A-D])/gm
+  const read = text.match(reg).toString()
+  console.log(read)
+  /*res.writeHead(200,{
+     'Content-Type' : 'application/mpegurl'
+    })*/
+  res.contentType('application/x-mpegURL')
+  res.end(`#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/en/4/42/Khorkuto.jpg" group-title="Bangladesh",Khorkuto
+  ${read}
+  `)
+  console.log('published')
+  }
+  
+
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ test.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs")
+const pretty = require("pretty");
+
+(async function(){
+  const url = "https://www.bdstory.xyz/search/label/khorkuto?m=1"
+  const res = await axios.get(url)
+  var $ = cheerio.load(res.data);
+  const link = $("#Blog1 > div.blog-posts.hfeed.container.post-filter-wrap > div > article > div > div.FreeGalxzw-box > h2 > a").attr('href')
+  console.log(`todays link ${link}`)
+  
+  const date = new Date().getDate()
+  
+  const ress = await axios.get(link)
+  $ = cheerio.load(ress.data);
+  const post = $("#Blog1 > div > article > div.post-inner-area > div.post-snip > span.post-date.published").text()
+  const pb_pst = post.split(" ")[1].split(',')[0]
+  console.log(pb_pst,date)
+  if(pb_pst != date){
+    console.log('not published')
+    console.log('https://github.com/sploit30/emni/blob/main/Untitled%2011%201280x720%201.42Mbps%202021-12-30%2017-16-47.mp4?raw=true')
+  }else{
+  const next = $("body").find("#BLOGGER-video-fbd948bdff01633a-15883").attr("src")
+  console.log(`next part ${next}`)
+  const neurl = await axios.get(next)
+  //console.log(neurl.data)
+  $ = cheerio.load(neurl.data);
+  const data = $('script').html()
+  var text = data.toString()
+  console.log(text)
+  const regular = /(https:\/\/rr1---sn-[^\s]+[A-D])/gm
+  const read = text.match(regular).toString()
+  console.log(read)
+  console.log('published')
+  }
+  
+})()
